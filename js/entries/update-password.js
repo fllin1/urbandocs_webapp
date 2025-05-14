5a7a36c69cd900d78b54ee9242c2d30cf7c7a74c
Florent <118511007+fllin1@users.noreply.github.com>
2025-05-14 16:08:33 +0200

    Implement Forgot Password Feature

---
+++ js/entries/update-password.js
@@ -0,0 +1,6 @@
+import { initUpdatePasswordPage } from "../auth/update-password.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initUpdatePasswordPage();
+  console.log("Update password page initialized");
+});
